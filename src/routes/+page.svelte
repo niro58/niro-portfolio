b30c88e6eb51e9e33cebff4884a40e42993c3f5c
niro58 <nichita.roilean@gmail.com>
2025-02-24 22:03:17 +0100

    shad and sveltekit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Button from '$lib/components/ui/button/button.svelte';
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<Button>yo</Button>
+
+<div class="w-32 rounded-lg bg-green-50 p-5">yo2</div>
